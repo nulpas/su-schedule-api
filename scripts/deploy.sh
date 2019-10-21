363d8f66d9700acd71c903149d517516fa042535
Nul Pàs <manu@lunaeme.com>
2019-10-21 13:46:40 +0200

    Su Schedule API :: v.0.0.0-beta.9  · Test deploy.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 set -x
-echo 'Deploy ready'
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+
+cd public
+git init
+
+git remote add deploy "travis@website.com:/opt/website"
+git config user.name "Travis CI"
+git config user.email "your.email+travis@gmail.com"
+
+git add .
+git commit -m "Deploy"
+git push --force deploy master
