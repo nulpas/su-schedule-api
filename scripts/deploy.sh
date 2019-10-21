fee8261d9af977c7820be6859caed02016532ee5
Nul Pàs <manu@lunaeme.com>
2019-10-21 13:47:23 +0200

    Su Schedule API :: v.0.0.0-beta.9  · Test deploy.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ ssh-add ~/.ssh/id_rsa
 cd public
 git init
 
-git remote add deploy "travis@website.com:/opt/website"
+git remote add deploy "travis@website.com:/var/www/vaquerosyzapatillas.com/api"
 git config user.name "Travis CI"
 git config user.email "your.email+travis@gmail.com"
 
