2d8ea7b17538b573efdfcd891687fb995ea2ae5d
Nul Pàs <manu@lunaeme.com>
2019-10-22 13:30:19 +0200

    Su Schedule API :: v.0.0.0-beta.9  · Test deploy.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 set -x
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
 
-echo ${TRAVIS_BUILD_DIR}
-pwd
-ls -la ~/.ssh/
-cd public
-git init
+ssh travis@api.vaquerosyzapatillas.com -p 18665
+cd /var/www/vaquerosyzapatillas.com/api
+mkdir pollas
 
-git remote add deploy "travis@website.com:/var/www/vaquerosyzapatillas.com/api"
-git config user.name "Travis CI"
-git config user.email "your.email+travis@gmail.com"
-
-git add .
-git commit -m "Deploy"
-git push --force deploy master
+#eval "$(ssh-agent -s)"
+#ssh-add ~/.ssh/id_rsa
+#
+#echo ${TRAVIS_BUILD_DIR}
+#pwd
+#ls -la ~/.ssh/
+#cd public
+#git init
+#
+#git remote add deploy "travis@website.com:/var/www/vaquerosyzapatillas.com/api"
+#git config user.name "Travis CI"
+#git config user.email "your.email+travis@gmail.com"
+#
+#git add .
+#git commit -m "Deploy"
+#git push --force deploy master
