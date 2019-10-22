84ab1e983c0e37e79e0aa4d8426fdf86aff84ccd
Nul Pàs <manu@lunaeme.com>
2019-10-22 10:44:29 +0200

    Su Schedule API :: v.0.0.0-beta.9  · Test deploy.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,6 @@ git init
 git remote add deploy "travis@website.com:/var/www/vaquerosyzapatillas.com/api"
 git config user.name "Travis CI"
 git config user.email "your.email+travis@gmail.com"
-
 git add .
 git commit -m "Deploy"
 git push --force deploy master
