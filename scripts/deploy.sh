cbda0bc4f2a2ffe4054c385dd9e6c103462855f4
Nul Pàs <manu@lunaeme.com>
2019-10-23 10:15:58 +0200

    Su Schedule API :: v.0.0.0-beta.9  · Completed deploy process using Travis.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,20 +23,3 @@ ssh -o "StrictHostKeyChecking no" travis@188.166.18.204 -p 18665 /bin/bash << EO
   pm2 stop vyz.js -s
   pm2 start vyz.js --env production
 EOF
-
-#eval "$(ssh-agent -s)"
-#ssh-add ~/.ssh/id_rsa
-#
-#echo ${TRAVIS_BUILD_DIR}
-#pwd
-#ls -la ~/.ssh/
-#cd public
-#git init
-#
-#git remote add deploy "travis@website.com:/var/www/vaquerosyzapatillas.com/api"
-#git config user.name "Travis CI"
-#git config user.email "your.email+travis@gmail.com"
-#
-#git add .
-#git commit -m "Deploy"
-#git push --force deploy master
