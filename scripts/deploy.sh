d1c57b9a65801d61b67ce499d8fe29d988e297cc
Nul Pàs <manu@lunaeme.com>
2020-02-09 20:14:15 +0100

    Su Schedule API :: v.0.0.0-beta.22  · Set Docker deploy production.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ set -x
 ssh -o "StrictHostKeyChecking no" travis@188.166.18.204 -p 18665 /bin/bash << EOF
   cd /var/www/vaquerosyzapatillas.com/api
   docker login repo.treescale.com --username=nulpas --password=$TREESCALE_PS
-  docker rm repo.treescale.com/nulpas/vyz-api:latest --force
+  docker rm vyz --force
   docker pull repo.treescale.com/nulpas/vyz-api:latest
   docker run --name vyz -d -p 4001:4001 --network="host" repo.treescale.com/nulpas/vyz-api:latest
 EOF
